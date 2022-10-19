bfb36171e2496295aa22eb5e6bb38eea6cd83ca9
Agustin Di Nezio <adineziocamiletti@gmail.com>
2022-10-19 17:34:24 -0300

    Clase_19_10

---
+++ WindowsApp/frmLoop.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace WindowsApp
+{
+    public partial class frmLoop : Form
+    {
+        public frmLoop()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFormLoop_Click(object sender, EventArgs e)
+        {
+
+            int total = 0;
+            MessageBox.Show("Ingresar 4 numeros menores a 100");
+            for (int i = 1; i <=4 ; i++)
+            {
+                int numero = Int32.Parse(Interaction.InputBox("Ingrese numero : ","Carga de numeros"));
+                while (numero >=100)
+                {
+                    MessageBox.Show("El numero ingresado es mayor que 100.");
+                    numero = Int32.Parse(Interaction.InputBox("Ingrese numero : ", "Carga de numeros"));
+                }
+                total += numero;
+
+            }
+            MessageBox.Show($"La suma de todos los numeros es : {total}");
+        }
+    }
+}
---
+++ WindowsApp/frmMetodos.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsApp
+{
+    public partial class frmMetodos : Form
+    {
+        public frmMetodos()
+        {
+            InitializeComponent();
+        }
+
+        private void btnImprimir_Click(object sender, EventArgs e)
+        {
+            Imprimir("Agustin");
+            int resultado = Suma(10, 20);
+            MessageBox.Show(resultado.ToString());
+        }
+        private void btnMostrarMensaje_Click(object sender, EventArgs e)
+        {
+            Imprimir("Agustin", "Di Nezio");
+        }
+
+        //Mis Funciones void
+        //Metodo creado con inteligencia artificial.
+        private static void Imprimir()
+        {
+            MessageBox.Show("Bienvenido");
+        }
+  
+        private static void Imprimir(string nombre)
+        {
+            MessageBox.Show($"Bienvenido {nombre}");
+        }
+
+        private static void Imprimir(string nombre, string apellido)
+        {
+            MessageBox.Show($"Bienvenido {nombre} {apellido}");
+        }
+       
+        //Mis funciones con retorno
+        private int Suma(int num1,int num2) =>  num1 + num2;
+
+        private int Suma(int num1, int num2, int num3) => num1 + num2 + num3;
+
+    }
+}
