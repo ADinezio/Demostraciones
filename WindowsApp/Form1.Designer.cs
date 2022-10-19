b5af84a0ece99036024d1735313551584b929ee5
Agustin Di Nezio <adineziocamiletti@gmail.com>
2022-10-19 14:39:06 -0300

    Agregar archivos de proyecto.

---
+++ WindowsApp/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WindowsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(207, 90);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(47, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Nombre:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(207, 145);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(64, 13);
+            this.lblPassword.TabIndex = 1;
+            this.lblPassword.Text = "Contraseña:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(293, 83);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(293, 137);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(293, 192);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(75, 23);
+            this.btnIngresar.TabIndex = 4;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 408);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnIngresar;
+    }
+}
+
---
+++ WindowsApp/Form2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsApp
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            double nota1 = Convert.ToDouble(txtNota1.Text);
+            double nota2 = Convert.ToDouble(txtNota2.Text);
+            double nota3 = Convert.ToDouble(txtNota3.Text);
+            double nota4 = Convert.ToDouble(txtNota4.Text);
+            double[] notas = new double[4] { nota1, nota2, nota3, nota4 };
+            double mayor = 0;
+            double promedio = 0;
+            double suma = 0;
+            for(int i = 0; i < notas.Length; i++)
+            { 
+                if (notas[i]>mayor) mayor = notas[i];
+                suma+=notas[i];
+            }
+            promedio = suma / 4;
+
+            if (promedio >= 9) MessageBox.Show($"Mayor nota: {mayor}\nPromedio: {promedio}\nDevolucion: Exelente!");
+            else if (promedio < 9 && promedio >= 7) MessageBox.Show($"Mayor nota: {mayor}\nPromedio: {promedio}\nDevolucion: Muy Bien!");
+            else if (promedio < 7 && promedio >= 4) MessageBox.Show($"Mayor nota: {mayor}\nPromedio: {promedio}\nDevolucion: Regular.");
+            else MessageBox.Show($"Mayor nota: {mayor}\nPromedio: {promedio}\nDevolucion: En proceso de aprendizaje.");
+  
+        }
+    }
+}
